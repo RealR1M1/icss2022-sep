4299080c9d5059a4793ab6f460e02bd15d511c80
Robin vanDommelen <nospam.nirimi@gmail.com>
2024-10-14 12:20:42 +0200

    Update Checker.java
    still have to clean up the declaration function, but it's functional at least

--- startcode/src/main/java/nl/han/ica/icss/generator/checker/Checker.java
+++ startcode/src/main/java/nl/han/ica/icss/generator/checker/Checker.java
@@ -1,5 +1,7 @@
 package nl.han.ica.icss.generator.checker;
 
+import nl.han.ica.datastructures.HANLinkedList;
+import nl.han.ica.datastructures.HANListNode;
 import nl.han.ica.datastructures.IHANLinkedList;
 import nl.han.ica.icss.ast.*;
 import nl.han.ica.icss.ast.literals.ColorLiteral;
@@ -14,16 +16,48 @@ import java.util.HashMap;
 public class Checker {
 
     private IHANLinkedList<HashMap<String, ExpressionType>> variableTypes;
+    private HashMap<String, ExpressionType> map = new HashMap<>();;
 
     public void check(AST ast) {
-        // variableTypes = new HANLinkedList<>();
+        variableTypes = new HANLinkedList<>();
 
         checkStylesheet(ast.root);
 
     }
 
     private void checkStylesheet(Stylesheet node){
-        checkStylerule((Stylerule) node.getChildren().get(0));
+        for (ASTNode child : node.getChildren()){
+
+            if (child instanceof VariableAssignment){
+                checkVariableAssignment((VariableAssignment) child, map);
+            }
+
+            if (child instanceof Stylerule){
+                checkStylerule((Stylerule) child);
+            }
+        }
+    }
+
+    private void checkVariableAssignment(VariableAssignment node, HashMap<String, ExpressionType> map){
+        if (node.expression == null){
+            node.setError("Variable assignment must have an expression");
+        } else {
+            map.put(node.name.name, getExpressionType(node.expression));
+            variableTypes.addFirst(map);
+        }
+    }
+
+    private ExpressionType getExpressionType(Expression expression) {
+        ExpressionType type = null;
+
+        if (expression instanceof ColorLiteral){
+            type = ExpressionType.COLOR;
+
+        } else if (expression instanceof PixelLiteral){
+            type = ExpressionType.PIXEL;
+        }
+
+        return type;
     }
 
     private void checkStylerule(Stylerule node) {
@@ -36,11 +70,28 @@ public class Checker {
 
     private void checkDeclaration(Declaration node) {
         if (node.property.name.equals("width")) {
-            if (!(node.expression instanceof PixelLiteral)) {
+            if (node.expression instanceof VariableReference) {
+                HashMap <String, ExpressionType> map = variableTypes.get(0);
+                String varName = ((VariableReference) node.expression).name;
+                ExpressionType type = map.get(varName);
+
+                if (!type.equals(ExpressionType.PIXEL)) {
+                    node.setError("Property 'width' has wrong type");
+                }
+            } else if (!(node.expression instanceof PixelLiteral)) {
                 node.setError("Property 'width' has wrong type");
             }
+
         } else if (node.property.name.equals("color")) {
-            if (!(node.expression instanceof ColorLiteral)) {
+            if (node.expression instanceof VariableReference) {
+                HashMap <String, ExpressionType> map = variableTypes.get(0);
+                String varName = ((VariableReference) node.expression).name;
+                ExpressionType type = map.get(varName);
+
+                if (!type.equals(ExpressionType.COLOR)) {
+                    node.setError("Property 'color' has wrong type");
+                }
+            } else if (!(node.expression instanceof ColorLiteral)) {
                 node.setError("Property 'color' has wrong type");
             }
         }
